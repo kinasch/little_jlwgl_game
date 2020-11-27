713cb84c59034a879b647f342895f7114e3f4d23
kinasch <janik@schoenberg.name>
2020-11-27 20:54:52 +0100

    entities and camera

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -8,10 +8,12 @@ out vec3 color;
 out vec2 pass_textureCoords;
 
 uniform mat4 transformationMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
 
 void main(){
 
-    gl_Position = transformationMatrix * vec4(position,1.0);
+    gl_Position = projectionMatrix * viewMatrix *  transformationMatrix * vec4(position,1.0);
     color = vec3(position.x+0.5,1.0,position.y+0.5);
     pass_textureCoords = textureCoords;
 
