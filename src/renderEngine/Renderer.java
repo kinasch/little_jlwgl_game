af6ddaae690edb376c4a7864b62c40e80f85cc81
Janik Schoenberg <janik@schoenberg.name>
2021-01-05 22:03:12 +0100

    obj loader

--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -26,7 +26,7 @@ public class Renderer {
     public void prepare(){
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-        GL11.glClearColor(0,0,0.8f,1);
+        GL11.glClearColor(0,0,0,1);
     }
 
     public void render(Entity entity, StaticShader shader){
